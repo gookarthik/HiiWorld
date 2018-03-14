03afeb3e90057e0a28920f98314268cc7dfd30e6
gookarthik <35719907+gookarthik@users.noreply.github.com>
2018-03-14 15:27:59 +0530

    Update Generic2.java

--- Generic2.java
+++ Generic2.java
@@ -3,7 +3,7 @@ public class Generic2 {
 	static <T> void genericDisplay(T element)
 	{
 		System.out.println(element.getClass().getName()+"="+element);
-	}
+	
 	public static void main(String[] args) 
 	{
 		genericDisplay(16);
