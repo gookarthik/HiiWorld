78b12f7fd45e544ba9d33ec6133835b53e394627
gookarthik <35719907+gookarthik@users.noreply.github.com>
2018-05-14 12:43:00 +0530

    Add files via upload

---
+++ TryFinal.java
@@ -0,0 +1,10 @@
+
+
+public class TryFinal {
+	public void calc(int a,int b) {// throws ArithmeticException {
+		int div;
+		div = a/b;
+		System.out.println("SUM : " + div);
+	}
+	
+}
---
+++ TryInitiates.java
@@ -0,0 +1,16 @@
+
+public class TryInitiates {
+	
+	public static void main(String[] args) {
+		
+		//try {
+		TryFinal obj = new TryFinal();
+		int a = 5, b=0;
+		obj.calc(a, b);
+		}
+		//catch(ArithmeticException e) {
+			//System.out.println("karthikWorld");
+	//	}
+		
+	//}
+}
